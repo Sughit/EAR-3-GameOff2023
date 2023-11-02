29083974125729b3a7f8c18905d3a34c24615f86
emanuel5867 <emisor.2009@gmail.com>
2023-11-02 22:12:54 +0200

    meniu

---
+++ EAR-3-GameOff2023-game/Assets/mainMenuScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+
+    public void SettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void SettingsMenuBack()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
