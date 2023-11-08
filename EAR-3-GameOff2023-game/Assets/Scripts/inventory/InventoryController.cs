590e5a1e725fbfae1d02ea59d1d6bd0dd9d5af46
emanuel5867 <emisor.2009@gmail.com>
2023-11-08 22:44:30 +0200

    buton delete

--- EAR-3-GameOff2023-game/Assets/Scripts/inventory/InventoryController.cs
+++ EAR-3-GameOff2023-game/Assets/Scripts/inventory/InventoryController.cs
@@ -191,4 +191,9 @@ public class InventoryController : MonoBehaviour
             rectTransform.position = Input.mousePosition;
         }
     }
+
+    public void Delete()
+    {
+        Destroy(selectedItem.gameObject);
+    }
 }
