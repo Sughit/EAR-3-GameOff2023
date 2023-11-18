b400a188802d965eb9b083cc2cbebf72336910c7
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2023-11-18 15:19:29 +0200

    rotating item

--- EAR-3-GameOff2023-game/Assets/Scripts/inventory/InventoryItem.cs
+++ EAR-3-GameOff2023-game/Assets/Scripts/inventory/InventoryItem.cs
@@ -6,9 +6,31 @@ using UnityEngine.UI;
 public class InventoryItem : MonoBehaviour
 {
     public ItemData itemData;
+    public int HEIGHT
+    {
+        get
+        {
+            if(rotated == false)
+            {
+                return itemData.height;
+            }
+            return itemData.width;
+        }
+    }
+    public int WIDTH
+    {
+        get
+        {
+            if(rotated == false)
+            {
+                return itemData.width;
+            }
+            return itemData.height;
+        }
+    }
     public int onGridPositionX;
     public int onGridPositionY;
-
+    public bool rotated = false;
     internal void Set(ItemData itemData)
     {
         this.itemData = itemData;
@@ -16,8 +38,16 @@ public class InventoryItem : MonoBehaviour
         GetComponent<Image>().sprite = itemData.itemIcon;
 
         Vector2 size = new Vector2();
-        size.x = itemData.width * ItemGrid.tileSizeWidth;
-        size.y = itemData.height * ItemGrid.tileSizeHeight;
+        size.x = WIDTH * ItemGrid.tileSizeWidth;
+        size.y = HEIGHT * ItemGrid.tileSizeHeight;
         GetComponent<RectTransform>().sizeDelta = size;
+    }
+
+    internal void Rotate()
+    {
+        rotated = !rotated;
+
+        RectTransform rectTransform =GetComponent<RectTransform>();
+        rectTransform.rotation = Quaternion.Euler(0, 0, rotated == true ? 90f : 0f);
     }
 }
