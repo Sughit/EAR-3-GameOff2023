acb0af21a6049fc0767b18be8bfa93fce7fe2f14
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2023-11-25 20:32:29 +0200

    schimbat drastic cum functioneaza upgradeul de undita

---
+++ EAR-3-GameOff2023-game/Assets/Scripts/FishSpawnerIII.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawnerIII : MonoBehaviour
+{
+    public GameObject fish;
+    public GameObject goldFish;
+    public float minTimeToSpawn;
+    public float maxTimeToSpawn;
+    float timeToSpawn;
+    float currentTime;
+
+    void Start()
+    {
+        timeToSpawn=Random.Range(minTimeToSpawn, maxTimeToSpawn);
+        currentTime=timeToSpawn;
+    }
+
+    void Update()
+    {
+        if(upgradeManager.rodII)
+        {
+            if(currentTime<=0)
+        {
+            if(DayManager.dayNum==10 && Random.Range(1,5)==2)
+            {
+                Instantiate(goldFish,transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(fish,transform.position, Quaternion.identity);
+            }
+            currentTime=Random.Range(minTimeToSpawn, maxTimeToSpawn);
+        }
+        else
+        {
+            currentTime-=Time.deltaTime;
+        }
+        }
+    }
+}
